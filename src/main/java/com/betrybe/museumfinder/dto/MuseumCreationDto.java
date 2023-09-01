712b7c8bdf500936dd94c9add191c853836ea779
Pedro Viana <Pedro Viana>
2023-09-01 17:17:52 -0300

    chore/feat: class MuseumDto, MusemCreationDto, Model Museum & Actuator

---
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumCreationDto.java
@@ -0,0 +1,12 @@
+package com.betrybe.museumfinder.dto;
+
+import com.betrybe.museumfinder.model.Coordinate;
+
+/**
+ * Class MuseumCreationDto.
+ */
+public record MuseumCreationDto(String name, String description, String address,
+                                String collectionType, String subject, String url,
+                                Coordinate coordinate) {
+
+}
---
+++ src/main/java/com/betrybe/museumfinder/dto/MuseumDto.java
@@ -0,0 +1,11 @@
+package com.betrybe.museumfinder.dto;
+
+import com.betrybe.museumfinder.model.Coordinate;
+
+/**
+ * Class MuseumDto.
+ */
+public record MuseumDto(Long id, String name, String description, String address,
+                        String collectionType, String subject, String url, Coordinate coordinate) {
+
+}
